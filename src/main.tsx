266a08066b1c49a002035d34023857e3506cc98f
SimSoLAB <simon.feicks@additivestream.com>
2026-09-25 01:59:04 +0200

    working-system: install semantic event runtime

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { installImpactRuntime } from './impact/runtime.ts'
 import './index.css'
+
+function ImpactRuntimeBootstrap() {
+  useEffect(() => installImpactRuntime(), [])
+  return null
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <ImpactRuntimeBootstrap />
     <App />
   </React.StrictMode>,
 )
